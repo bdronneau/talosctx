378e1867edf4a7a88015cee9dc60a0d3dc51d605
bdronneau <basti1.dr@gmail.com>
2026-06-12 23:05:02 +0200

    tests: try fix flaky

--- internal/printer/main.go
+++ internal/printer/main.go
@@ -1,16 +1,20 @@
 package printer
 
 import (
+	"sort"
+
 	"github.com/bdronneau/talosctx/internal/talosconfig"
 	"github.com/pterm/pterm"
 )
 
 func SelectContext(data map[string]talosconfig.Taloscontext) (string, error) {
-	var entries []string
+	entries := make([]string, 0, len(data))
 
 	for entry := range data {
 		entries = append(entries, entry)
 	}
+
+	sort.Strings(entries)
 
 	selectedOption, err := pterm.DefaultInteractiveSelect.WithOptions(entries).Show()
 	if err != nil {
